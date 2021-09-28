97c0c131746d5f8366a6e6fe2bf71e560e96f4af
Eugene Shemetov <emshemetov@gmail.com>
2021-09-28 16:41:08 +0300

    Initial commit

---
+++ force-app/main/default/lwc/currencyRates/currencyRates.js
@@ -0,0 +1,10 @@
+/**
+ * Created by eshemetov on 28/09/2021.
+ */
+
+import {LightningElement, track} from 'lwc';
+
+export default class CurrencyRates extends LightningElement {
+    @track currencyList = [];
+
+}
