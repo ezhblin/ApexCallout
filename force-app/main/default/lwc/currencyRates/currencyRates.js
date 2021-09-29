3af1e7458bdd9d3715034b1a524524e48159b028
Eugene Shemetov <emshemetov@gmail.com>
2021-09-29 12:12:05 +0300

    task 1 finished

--- force-app/main/default/lwc/currencyRates/currencyRates.js
+++ force-app/main/default/lwc/currencyRates/currencyRates.js
@@ -1,10 +1,29 @@
 /**
  * Created by eshemetov on 28/09/2021.
  */
-
+import getRates from "@salesforce/apex/CurrencyRatesController.getRates";
 import {LightningElement, track} from 'lwc';
 
 export default class CurrencyRates extends LightningElement {
-    @track currencyList = [];
+    @track dataRates;
+    chosenDate;
+    spinner = false;
+
+    requestRates() {
+        this.spinner = true;
+        getRates({chosenDate : this.chosenDate})
+            .then(result => {
+                this.dataRates = JSON.parse(result);
+                this.spinner = false;
+            })
+            .catch(error => {
+                console.log('Something wrong!');
+                this.spinner = false;
+            });
+    }
+
+    setValue(event) {
+        this.chosenDate = event.target.value;
+    }
 
 }
